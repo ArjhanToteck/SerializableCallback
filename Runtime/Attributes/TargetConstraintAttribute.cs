41601d6e97390eb1f7cb9442def52cc1a1655679
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2023-05-07 21:35:24 -0600

    Put the whole package in a namespace
    Everything is now inside of a SerializableCallback and a SerializableCallback.Editor namespace

--- Runtime/Attributes/TargetConstraintAttribute.cs
+++ Runtime/Attributes/TargetConstraintAttribute.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System;
 
-/// <summary> Add to fields of your class extending SerializableCallbackBase<T,..> to limit which types can be assigned to it. </summary>
-public class TargetConstraintAttribute : PropertyAttribute {
-	public Type targetType;
-
+namespace SerializableCallback {
 	/// <summary> Add to fields of your class extending SerializableCallbackBase<T,..> to limit which types can be assigned to it. </summary>
-	public TargetConstraintAttribute(Type targetType) {
-		this.targetType = targetType;
+	public class TargetConstraintAttribute : PropertyAttribute {
+		public Type targetType;
+
+		/// <summary> Add to fields of your class extending SerializableCallbackBase<T,..> to limit which types can be assigned to it. </summary>
+		public TargetConstraintAttribute(Type targetType) {
+			this.targetType = targetType;
+		}
 	}
 }
